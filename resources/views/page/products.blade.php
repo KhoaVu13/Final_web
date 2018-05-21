3c87a6e0446ed2e7189208d106056600d303c811
Quốc Huy <daigia.sock@yahoo.com>
2018-05-21 23:16:52 +0700

    Phân trang

--- resources/views/page/products.blade.php
+++ resources/views/page/products.blade.php
@@ -24,12 +24,7 @@
 						<hr>
 						<div class="pagination pagination-small pagination-centered">
 							<ul>
-								<li><a href="#">Prev</a></li>
-								<li class="active"><a href="#">1</a></li>
-								<li><a href="#">2</a></li>
-								<li><a href="#">3</a></li>
-								<li><a href="#">4</a></li>
-								<li><a href="#">Next</a></li>
+								<div>{{$sp_theoloai->links()}}</div>
 							</ul>
 						</div>
 					</div>
